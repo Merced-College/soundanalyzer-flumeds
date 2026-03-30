a2ff96391e9187f368c02afc52b82a65f984e3b4
flumeds <113043505+flumeds@users.noreply.github.com>
2026-03-30 11:05:05 -0700

    Update SoundAnalyzer.cpp

--- SoundAnalyzer.cpp
+++ SoundAnalyzer.cpp
@@ -2,14 +2,17 @@
 
 // Constructor
 SoundAnalyzer::SoundAnalyzer(int quiet, int medium) {
-    // TODO: store threshold values
+    quietThreshold = quiet;
+    mediumThreshold = medium;
 }
 
 // Classify sound
 std::string SoundAnalyzer::classifySound(int soundLevel) {
-    // TODO:
-    // Use thresholds to return:
-    // "Quiet", "Medium", or "Loud"
-
-    return "Unknown"; // placeholder
+    if (soundLevel < quietThreshold) {
+        return "Quiet";
+    } else if (soundLevel < mediumThreshold) {
+        return "Medium";
+    } else {
+        return "Loud";
+    }
 }
