c65952b7f8f3ffc8133f8755905fbe5060b2169b
flumeds <113043505+flumeds@users.noreply.github.com>
2026-03-30 11:03:45 -0700

    Update SoundSensor.cpp

--- SoundSensor.cpp
+++ SoundSensor.cpp
@@ -1,21 +1,19 @@
 #include "SoundSensor.h"
 #include <Arduino.h>
 
-// Constructor
 SoundSensor::SoundSensor(int sensorPin) {
-    // TODO: store the pin value
+    pin = sensorPin;
 }
 
-// Initialize sensor
 void SoundSensor::begin() {
-    // TODO: set pin mode if needed
+    pinMode(pin, INPUT);
 }
 
-// Read sound level
 int SoundSensor::getSoundLevel() {
-    // TODO:
-    // - read from analog pin
-    // - return the value
+    return analogRead(pin);
+}
 
-    return 0; // placeholder
+// ADD THIS
+bool SoundSensor::isLoud(int threshold) {
+    return getSoundLevel() > threshold;
 }
